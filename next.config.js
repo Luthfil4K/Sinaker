b40744c364887d25db3dfbc1543d5a47a774398c
LuthfilKA <222011321@stis.ac.id>
2024-02-22 13:25:01 +0700

    M : next config js, tambah page extensions buat handle error pas deploy vercell (topsis eror)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   experimental: {
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
