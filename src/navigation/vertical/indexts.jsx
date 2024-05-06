cab97dc0ed31da86bdb23c17b99ff52127604510
LuthfilKA <222011321@stis.ac.id>
2024-05-06 17:39:06 +0700

    M/A : error di dash menu

--- src/navigation/vertical/indexts.jsx
+++ src/navigation/vertical/indexts.jsx
@@ -31,6 +31,8 @@ const navigation = () => {
     setUserRole(session?.data?.role)
   }
 
+  console.log(session.data.role)
+
   useEffect(() => {
     if (session.status === 'authenticated') {
       getUserRole()
@@ -311,6 +313,211 @@ const navigation = () => {
         title: 'Daftar Mitra',
         icon: Mitra,
         path: '/mitra'
+      }
+    ]
+  } else if (userRole == 'admin') {
+    return [
+      {
+        title: 'Dashboard',
+        icon: HomeOutline,
+        path: '/'
+      },
+      {
+        title: 'Account Settings',
+        icon: AccountCogOutline,
+        path: '/account-settings'
+      },
+      {
+        sectionTitle: 'Pages '
+      },
+      {
+        title: 'Login',
+        icon: Login,
+        path: '/pages/login',
+        openInNewTab: true
+      },
+      {
+        title: 'Register',
+        icon: AccountPlusOutline,
+        path: '/pages/register',
+        openInNewTab: true
+      },
+      {
+        title: 'Error',
+        icon: AlertCircleOutline,
+        path: '/pages/error',
+        openInNewTab: true
+      },
+
+      {
+        title: 'Dashboard',
+        icon: HomeOutline,
+        path: '/'
+      },
+
+      {
+        title: 'Pengumuman',
+        icon: CubeOutline,
+        path: '/pengumuman'
+      },
+      {
+        title: 'Timeline',
+        icon: CubeOutline,
+        path: '/timeline'
+      },
+      {
+        sectionTitle: 'Rapat'
+      },
+      {
+        title: 'Ajukan Rapat',
+        icon: Login,
+        path: '/rapat-create'
+      },
+      {
+        title: 'Daftar Rapat',
+        icon: Login,
+        path: '/rapat-ajuan-list'
+      },
+      {
+        sectionTitle: 'Kegiatan'
+      },
+      {
+        title: 'Buat Kegiatan',
+        icon: CreditCardOutline,
+        path: '/create-project'
+      },
+
+      {
+        title: 'List Kegiatan',
+        icon: Table,
+        path: '/project-list'
+      },
+      {
+        title: 'Sub Kegiatan',
+        icon: CubeOutline,
+        path: '/task'
+      },
+
+      {
+        sectionTitle: 'Pencairan'
+      },
+      {
+        title: 'Pencairan',
+        icon: Cash,
+        path: '/pencairan'
+      },
+
+      {
+        sectionTitle: 'Tim Kerja'
+      },
+      {
+        title: 'Tim Kerja',
+        icon: CubeOutline,
+        path: '/tim-kerja-list'
+      },
+      {
+        title: 'Buat Tim Kerja',
+        icon: Login,
+        path: '/create-tim-kerja'
+      },
+
+      {
+        sectionTitle: 'Perusahaan'
+      },
+      {
+        title: 'List Group Perusahaan',
+        icon: Table,
+        path: '/perusahaan-group-list'
+      },
+      {
+        title: 'Buat Group Perusahaan',
+        icon: Login,
+        path: '/create-kegiatan-perusahaan'
+      },
+
+      {
+        sectionTitle: 'Pustaka'
+      },
+      {
+        title: 'Beban Kerja',
+        icon: Elevator,
+        path: '/beban-kerja'
+      },
+      {
+        title: 'Daftar Pegawai',
+        icon: Account,
+        path: '/pegawai'
+      },
+      {
+        title: 'Daftar Perusahaan',
+        icon: GoogleCirclesExtended,
+        path: '/perusahaan'
+      },
+      {
+        title: 'Daftar Mitra',
+        icon: FormatLetterCase,
+        path: '/mitra'
+      },
+      {
+        sectionTitle: 'Pengaturan'
+      },
+      {
+        title: 'Pengaturan Bobot Kriteria',
+        icon: FilterSettings,
+        path: '/pengaturan-rekomendasi'
+      },
+      {
+        title: 'Table Kegiatan',
+        icon: Table,
+        path: '/template-table-list'
+      },
+
+      {
+        sectionTitle: 'Pustaka'
+      },
+      {
+        title: 'Induk Kegiatan',
+        icon: CubeOutline,
+        path: '/master-induk-kegiatan'
+      },
+      {
+        title: 'Kode',
+        icon: Login,
+        path: '/master-kode'
+      },
+
+      {
+        sectionTitle: 'User Interface'
+      },
+      {
+        title: 'Typography',
+        icon: FormatLetterCase,
+        path: '/typography'
+      },
+      {
+        title: 'Icons',
+        path: '/icons',
+        icon: GoogleCirclesExtended
+      },
+      {
+        title: 'Cards',
+        icon: CreditCardOutline,
+        path: '/cards'
+      },
+      {
+        title: 'Tables',
+        icon: Table,
+        path: '/tables'
+      },
+      {
+        icon: CubeOutline,
+        title: 'Form Layouts',
+        path: '/form-layouts'
+      },
+      {
+        icon: CubeOutline,
+        title: 'iccon',
+        path: '/iccon'
       }
     ]
   }
