61181f51576b43dc8cbc9d29e2def9a5212cae4f
LuthfilKA <222011321@stis.ac.id>
2024-06-26 15:53:57 +0700

    M : fix bug

--- src/views/rapat-views/RapatApproveList.jsx
+++ src/views/rapat-views/RapatApproveList.jsx
@@ -624,7 +624,10 @@ const RapatApproveList = props => {
                       : false,
                   uploadPersetujuan:
                     session.status === 'authenticated' &&
-                    (session.data.uid === 1099999 || session.data.role == 'pimpinan')
+                    (session.data.uid === 1099999 ||
+                      session.data.role == 'kepalaBPS' ||
+                      session.data.role == 'pimpinan' ||
+                      session.data.role == 'superAdmin')
                       ? true
                       : false
                 }}
